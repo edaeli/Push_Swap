822fadd6c27c9f511dfc30a79a165432383c212c
Khoren Ayvazyan <khoayvaz@c2r16s2.42yerevan.am>
2026-03-26 18:30:16 +0400

    push_swap

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: taslanya <taslanya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 20:06:14 by taslanya          #+#    #+#             */
-/*   Updated: 2026/03/26 18:18:16 by khoayvaz         ###   ########.fr       */
+/*   Updated: 2026/03/26 18:27:38 by khoayvaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,127 +14,101 @@
 
 void	print_bench(double disorder, char *strat, char *complexity)
 {
-write(2, "\n[bench] Disorder: ", 19);
-ft_putnbr_fd((int)(disorder * 100), 2);
-write(2, "%\n", 2);
-write(2, "[bench] Strategy: ", 18);
-write(2, strat, ft_strlen(strat));
-write(2, "\n[bench] Complexity: ", 21);
-write(2, complexity, ft_strlen(complexity));
-write(2, "\n", 1);
+	write(2, "\n[bench] Disorder: ", 19);
+	ft_putnbr_fd((int)(disorder * 100), 2);
+	write(2, "%\n", 2);
+	write(2, "[bench] Strategy: ", 18);
+	write(2, strat, ft_strlen(strat));
+	write(2, "\n[bench] Complexity: ", 21);
+	write(2, complexity, ft_strlen(complexity));
+	write(2, "\n", 1);
 }
 
-void	run_adaptive(t_node **a, t_node **b, double d)
+static void	get_bench_info(char *mode, double d, int size)
 {
-if (d < 0.2)
-simple_sort(a, b);
-else if (d < 0.5)
-chunk_sort(a, b);
-else
-radix_sort(a, b);
+	if (size <= 5)
+		print_bench(d, "Small Sort", "O(1)");
+	else if (ft_strcmp(mode, "--simple") == 0 || (ft_strcmp(mode,
+				"--adaptive") == 0 && d < 0.2))
+		print_bench(d, "Simple Sort", "O(n2)");
+	else if (ft_strcmp(mode, "--medium") == 0 || (ft_strcmp(mode,
+				"--adaptive") == 0 && d < 0.5))
+		print_bench(d, "Chunk Sort", "O(n*sqrt(n))");
+	else
+		print_bench(d, "Radix Sort", "O(n log n)");
+	count_op(NULL, 2);
+}
+
+static void	apply_sort(t_node **a, t_node **b, char *mode, double d)
+{
+	int	size;
+
+	size = stack_size(*a);
+	if (size == 2 && (*a)->index > (*a)->next->index)
+		sa(a);
+	else if (size == 3)
+		sort_3(a);
+	else if (size == 5)
+		sort_5(a, b);
+	else if (ft_strcmp(mode, "--simple") == 0)
+		simple_sort(a, b);
+	else if (ft_strcmp(mode, "--medium") == 0)
+		chunk_sort(a, b);
+	else if (ft_strcmp(mode, "--complex") == 0)
+		radix_sort(a, b);
+	else
+	{
+		if (d < 0.2)
+			simple_sort(a, b);
+		else if (d < 0.5)
+			chunk_sort(a, b);
+		else
+			radix_sort(a, b);
+	}
+}
+
+static int	parse_flags(int argc, char **argv, char **mode, int *bench)
+{
+	int	i;
+
+	i = 1;
+	*mode = "--adaptive";
+	*bench = 0;
+	while (i < argc && argv[i][0] == '-')
+	{
+		if (ft_strcmp(argv[i], "--bench") == 0)
+			*bench = 1;
+		else
+			*mode = argv[i];
+		i++;
+	}
+	return (i);
 }
 
 int	main(int argc, char **argv)
 {
-t_node	*a;
-t_node	*b;
-char	*mode;
-int		bench;
-int		i;
-int		size;
-double	disorder;
-char	*s_name;
-char	*c_class;
+	t_node	*stk[2];
+	char	*mode;
+	int		bench;
+	int		idx;
+	double	init_d;
 
-b = NULL;
-mode = "--adaptive";
-bench = 0;
-i = 1;
-if (argc < 2)
+	idx = parse_flags(argc, argv, &mode, &bench);
+	if (idx >= argc)
+		return (0);
+	stk[0] = parse_args(argc - idx + 1, &argv[idx - 1]);
+	index_stack(stk[0]);
+	if (is_sorted(stk[0]))
+	{
+		free_stack(&stk[0]);
+		return (0);
+	}
+	init_d = compute_disorder(stk[0]);
+	stk[1] = NULL;
+	apply_sort(&stk[0], &stk[1], mode, init_d);
+	if (bench)
+		get_bench_info(mode, init_d, stack_size(stk[0]));
+	free_stack(&stk[0]);
+	free_stack(&stk[1]);
 	return (0);
-while (i < argc && argv[i][0] == '-')
-{
-	if (ft_strcmp(argv[i], "--bench") == 0)
-		bench = 1;
-	else if (ft_strcmp(argv[i], "--simple") == 0)
-		mode = "--simple";
-	else if (ft_strcmp(argv[i], "--medium") == 0)
-		mode = "--medium";
-	else if (ft_strcmp(argv[i], "--complex") == 0)
-		mode = "--complex";
-	else if (ft_strcmp(argv[i], "--adaptive") == 0)
-		mode = "--adaptive";
-	i++;
-}
-if (i >= argc)
-	return (0);
-a = parse_args(argc - i + 1, &argv[i - 1]);
-index_stack(a);
-disorder = compute_disorder(a);
-size = stack_size(a);
-if (is_sorted(a))
-{
-	free_stack(&a);
-	return (0);
-}
-if (size == 2)
-	sa(&a);
-else if (size == 3)
-	sort_3(&a);
-else if (size == 5)
-	sort_5(&a, &b);
-else if (ft_strcmp(mode, "--simple") == 0)
-	simple_sort(&a, &b);
-else if (ft_strcmp(mode, "--medium") == 0)
-	chunk_sort(&a, &b);
-else if (ft_strcmp(mode, "--complex") == 0)
-	radix_sort(&a, &b);
-else
-	run_adaptive(&a, &b, disorder);
-if (bench)
-{
-	s_name = "Adaptive Selection";
-	c_class = "Mixed";
-	if (size <= 5)
-	{
-		s_name = "Small Sort";
-		c_class = "O(1)";
-	}
-	else if (ft_strcmp(mode, "--simple") == 0)
-	{
-		s_name = "Simple Sort";
-		c_class = "O(n2)";
-	}
-	else if (ft_strcmp(mode, "--medium") == 0)
-	{
-		s_name = "Chunk Sort";
-		c_class = "O(n*sqrt(n))";
-	}
-	else if (ft_strcmp(mode, "--complex") == 0)
-	{
-		s_name = "Radix Sort";
-		c_class = "O(n log n)";
-	}
-	else if (disorder < 0.2)
-	{
-		s_name = "Simple (Adaptive)";
-		c_class = "O(n2)";
-	}
-	else if (disorder < 0.5)
-	{
-		s_name = "Chunk (Adaptive)";
-		c_class = "O(n*sqrt(n))";
-	}
-	else
-	{
-		s_name = "Radix (Adaptive)";
-		c_class = "O(n log n)";
-	}
-	print_bench(disorder, s_name, c_class);
-	count_op(NULL, 2);
-}
-free_stack(&a);
-free_stack(&b);
-return (0);
-
 }
--- simple_sort.c
+++ simple_sort.c
@@ -6,26 +6,25 @@
 /*   By: taslanya <taslanya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 20:39:16 by taslanya          #+#    #+#             */
-/*   Updated: 2026/03/26 18:00:38 by khoayvaz         ###   ########.fr       */
+/*   Updated: 2026/03/26 18:24:38 by khoayvaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "push_swap.h"
 
 int	find_min_pos(t_node *stack)
 {
-	int	min_index;
+	int	min_idx;
 	int	pos;
 	int	i;
 
-	min_index = stack->index;
+	min_idx = stack->index;
 	pos = 0;
 	i = 0;
 	while (stack)
 	{
-		if (stack->index < min_index)
+		if (stack->index < min_idx)
 		{
-			min_index = stack->index;
+			min_idx = stack->index;
 			pos = i;
 		}
 		stack = stack->next;
@@ -36,18 +35,18 @@ int	find_min_pos(t_node *stack)
 
 int	find_max_pos(t_node *stack)
 {
-	int	max_index;
+	int	max_idx;
 	int	pos;
 	int	i;
 
-	max_index = stack->index;
+	max_idx = stack->index;
 	pos = 0;
 	i = 0;
 	while (stack)
 	{
-		if (stack->index > max_index)
+		if (stack->index > max_idx)
 		{
-			max_index = stack->index;
+			max_idx = stack->index;
 			pos = i;
 		}
 		stack = stack->next;
@@ -58,26 +57,27 @@ int	find_max_pos(t_node *stack)
 
 void	simple_sort(t_node **a, t_node **b)
 {
-	int	min_pos;
+	int	pos;
 	int	size;
 
-	while (!is_sorted(*a))
+	while (stack_size(*a) > 3 && !is_sorted(*a))
 	{
 		size = stack_size(*a);
-		min_pos = find_min_pos(*a);
-		if (min_pos <= size / 2)
+		pos = find_min_pos(*a);
+		if (pos <= size / 2)
 		{
-			while (min_pos-- > 0)
+			while (pos-- > 0)
 				ra(a);
 		}
 		else
 		{
-			min_pos = size - min_pos;
-			while (min_pos-- > 0)
+			pos = size - pos;
+			while (pos-- > 0)
 				rra(a);
 		}
 		pb(a, b);
 	}
+	sort_3(a);
 	while (*b)
 		pa(a, b);
 }
